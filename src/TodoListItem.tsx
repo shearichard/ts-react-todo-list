fd5a96eae2625a5a595d1b3809083617912061e9
Richard Shea <rshea@thecubagroup.com>
2020-12-14 23:03:10 +1300

    Third stage of 'TodoListItem'

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -8,6 +8,14 @@ interface Todo {
 interface Props {
   todo: Todo;    
 }
-export const TodoListItem: React.FC<Props> = props => {
-  return <li>content</li>;
+export const TodoListItem: React.FC<Props> = ({todo})=> {
+  return (
+    <li>
+      <label
+        style={{ textDecoration: todo.complete ? 'line-through' : undefined }}
+      >
+        <input type="checkbox" checked={todo.complete} /> {todo.text}
+      </label>
+    </li>
+  );
 };
