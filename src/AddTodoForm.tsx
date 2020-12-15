732e4d816ff38440cff29f64e2c65209cf1c90d5
Richard Shea <rshea@thecubagroup.com>
2020-12-16 11:42:52 +1300

    Wired up the form so that items entered are added to the list of the todos. This commit marks the finish of the walkthrough of the project.

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
 
-export const AddTodoForm: React.FC = () => {
+interface Props {
+    addTodo: AddTodo;
+}
+
+/*
+ 1. Maintain internal `text` state using `useState`. This will allow us to maintain the state of the new todo item’s text.
+ 2. Bind `text` to the `input` value.
+ 3. Set text using `setText` in the input’s `onChange` handler. `e.target.value` contains the current value.
+ 4. Add an `onClick` handler to the submit button to submit the typed text.
+ 5. Make sure to cancel the default event of actually submitting the form.
+ 6. Add the todo using `addTodo` and passing it `text`.
+ 7. Clearing our form by setting `text` to an empty string.
+*/
+
+export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
+
+  const [text, setText] = useState('');
+
   return (
     <form>
-      <input type="text" />
-      <button type="submit">Add Todo</button>
+      <input 
+        type="text" 
+        value={text}
+        onChange={e => {
+            setText(e.target.value);
+        }}
+      />
+      <button 
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          addTodo(text);
+          setText('');
+        }}
+      >
+        Add Todo
+      </button>
     </form>
   );
 };
